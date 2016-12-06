a89d4e8e89ab82a8851aadb90a9e776a955bdb14
Roel Zinkstok <rzinkstok@gmail.com>
2016-12-06 17:07:55 +0100

    Moves metapost file

--- skymap/metapost.py
+++ skymap/metapost.py
@@ -1,12 +1,19 @@
+import os
 import subprocess
 import shutil
 
 from geometry import Point
 
 
+class DrawError(Exception):
+    pass
+
+
 class MetaPostFigure(object):
     def __init__(self, name, comment=None):
         self.name = name
+        if not os.path.exists("mpost"):
+            os.makedirs("mpost")
         self.fp = open("mpost/{0}.mp".format(name), "w")
         self.delayed = []
         self.start_figure(comment)
@@ -50,10 +57,12 @@ class MetaPostFigure(object):
         s = "draw {0}--{1}--{2}--{3}--cycle withcolor {4} withpen pencircle scaled {5}pt;\n".format(c1, c2, c3, c4, color, linewidth)
         self.fp.write(s)
 
-    def draw_line(self, p1, p2, color="black", linewidth=0.5, dotted=False, delay_write=False):
+    def draw_line(self, line, color="black", linewidth=0.5, dotted=False, delay_write=False):
         # dotted: dashed withdots scaled 0.5
-        c1 = self.point_to_coordinates(p1)
-        c2 = self.point_to_coordinates(p2)
+        if not hasattr(line, "p1") or not hasattr(line, "p2"):
+            raise DrawError
+        c1 = self.point_to_coordinates(line.p1)
+        c2 = self.point_to_coordinates(line.p2)
         s = "draw {0}--{1} withcolor {2} withpen pencircle scaled {3}pt".format(c1, c2, color, linewidth)
         if dotted:
             s += " dashed withdots scaled 0.5"
@@ -63,7 +72,7 @@ class MetaPostFigure(object):
         else:
             self.fp.write(s)
 
-    def draw_polygon(self, points, color="black", linewidth=0.5, closed=True, dotted=False, delay_write=False):
+    def draw_polygon(self, points, color="black", linewidth=0.5, closed=True, dashed=False, dotted=False, delay_write=False):
         s = "draw "
         for p in points:
             c1 = self.point_to_coordinates(p)
@@ -75,6 +84,8 @@ class MetaPostFigure(object):
         s += " withcolor {0} withpen pencircle scaled {1}pt".format(color, linewidth)
         if dotted:
             s += " dashed withdots scaled {0}".format(linewidth)
+        if dashed:
+            s += " dashed evenly scaled {0}".format(linewidth)
         s += ";\n"
         if delay_write:
             self.delayed.append(s)
@@ -86,9 +97,31 @@ class MetaPostFigure(object):
         s = "draw {0} withcolor {1} withpen pencircle scaled {2}mm;\n".format(c, color, size)
         self.fp.write(s)
 
-    def draw_circle(self, p, radius, color="black", linewidth=0.5):
-        c = self.point_to_coordinates(p)
-        self.fp.write("draw fullcircle scaled {0}mm shifted {1} withcolor {2} withpen pencircle scaled {3}pt;\n".format(2*radius, c, color, linewidth))
+    def draw_circle(self, circle, color="black", linewidth=0.5, dotted=False, delay_write=False):
+        if not hasattr(circle, "center") or not hasattr(circle, "radius"):
+            raise DrawError
+        c = self.point_to_coordinates(circle.center)
+        s = "draw fullcircle scaled {0}mm shifted {1} withcolor {2} withpen pencircle scaled {3}pt".format(2*circle.radius, c, color, linewidth)
+        if dotted:
+            s += " dashed withdots scaled {}".format(linewidth)
+        s += ";\n"
+        if delay_write:
+            self.delayed.append(s)
+        else:
+            self.fp.write(s)
+
+    def draw_arc(self, arc, color="black", linewidth=0.5, dotted=False, delay_write=False):
+        if not hasattr(arc, "center") or not hasattr(arc, "radius") or not hasattr(arc, "start_mp"):
+            raise DrawError
+        c = self.point_to_coordinates(arc.center)
+        s = "draw subpath ({}, {}) of fullcircle scaled {}mm shifted {} withcolor {} withpen pencircle scaled {}pt".format(arc.start_mp, arc.stop_mp, 2*arc.radius, c, color, linewidth)
+        if dotted:
+            s += " dashed withdots scaled {}".format(linewidth)
+        s += ";\n"
+        if delay_write:
+            self.delayed.append(s)
+        else:
+            self.fp.write(s)
 
     def draw_text(self, p, text, pos, size="tiny", scale=None, angle=None, delay_write=False):
         o = self.point_to_coordinates(Point(0, 0))
@@ -117,9 +150,12 @@ class MetaPostFigure(object):
         self.fp.write("b := {0}--{1}--{2}--{3}--cycle;\n".format(c1, c2, c3, c4))
         self.fp.write("clip currentpicture to b;\n")
 
-    @staticmethod
-    def render(filename):
-        subprocess.Popen(["mpost", "map"], cwd="mpost").wait()
-        subprocess.Popen(["mptopdf map.1"], shell=True, cwd="mpost").wait()
-        shutil.move("mpost/map-1.pdf", filename)
+    def render(self, filename):
+        print os.environ['PATH']
+        subprocess.Popen(["mpost", self.name+".mp"], cwd="mpost").wait()
+        subprocess.Popen(["mptopdf {0}.1".format(self.name)], shell=True, cwd="mpost").wait()
+        folder = os.path.dirname(filename)
+        if folder and not os.path.exists(folder):
+            os.makedirs(folder)
+        shutil.move("mpost/{0}-1.pdf".format(self.name), filename)
         subprocess.Popen(["open", filename]).wait()
