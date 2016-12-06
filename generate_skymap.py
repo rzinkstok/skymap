3056b9128a27b14da54f0bb2a1a04ad628423b74
Roel Zinkstok <rzinkstok@gmail.com>
2016-12-06 19:15:43 +0100

    Adds atlas creation

--- generate_skymap.py
+++ generate_skymap.py
@@ -1,12 +1,27 @@
-from skymap.map import Map, AzimuthalEquidistantMap, EquidistantCylindricalMap, EquidistantConicMap
-from skymap.metapost import MetaPostFigure
-from skymap.geometry import Arc, Point
+import os
+from skymap.map import AzimuthalEquidistantMap, EquidistantCylindricalMap, EquidistantConicMap
 
-#m = AzimuthalEquidistantMap("test.pdf", north=True, celestial=True)
-#m = EquidistantCylindricalMap("test.pdf", celestial=True, reference_longitude=-15, reference_scale=30)
-m = EquidistantConicMap("test.pdf", celestial=True, standard_parallel1=25, standard_parallel2=47, reference_longitude=30)
-m.render()
 
+def build_atlas(path="atlas"):
+    if not os.path.exists(path):
+        os.makedirs(path)
+
+    m = AzimuthalEquidistantMap("atlas/01.pdf", north=True, celestial=True, reference_scale=52)
+    m.render()
+    m = AzimuthalEquidistantMap("atlas/20.pdf", north=False, celestial=True, reference_scale=-52)
+    m.render()
+
+    for i, central_longitude in enumerate(range(0, 360, 60)):
+        m = EquidistantConicMap("atlas/{0:02d}.pdf".format(i+2), celestial=True, standard_parallel1=28, standard_parallel2=52, reference_longitude=central_longitude, scale=1.25)
+        m.render()
+
+        m = EquidistantCylindricalMap("atlas/{0:02d}.pdf".format(i+8), celestial=True, reference_longitude=central_longitude, standard_parallel=14, reference_scale=30)
+        m.render()
+
+        m = EquidistantConicMap("atlas/{0:02d}.pdf".format(i+14), celestial=True, standard_parallel1=-28, standard_parallel2=-52, reference_longitude=central_longitude, scale=1.25)
+        m.render()
+
+build_atlas()
 
 
 
