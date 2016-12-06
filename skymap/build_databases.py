79ba473cd265245c1d358929a5297ef8cb058cdb
Roel Zinkstok <rzinkstok@gmail.com>
2016-12-06 17:07:27 +0100

    Moves build_databases file

--- skymap/build_databases.py
+++ skymap/build_databases.py
@@ -2,6 +2,6 @@ from hipparcos import build_hipparcos_database
 from hyg import build_hyg_database
 from constellations import build_constellation_database
 
-#build_constellation_database()
-#build_hipparcos_database()
+build_constellation_database()
+build_hipparcos_database()
 build_hyg_database()
